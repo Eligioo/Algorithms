f4f5b23ede9c379bcf673ce9702f307f6219959f
Stefan Koolen <nast3zz@gmail.com>
2016-12-02 15:01:27 +0100

    Implemented merge sort

--- MergeSort/Run.cs
+++ MergeSort/Run.cs
@@ -10,7 +10,55 @@ namespace MergeSort
     {
         static void Main(string[] args)
         {
-            
+            int[] unsortedArray = new int[] { 6, 3, 5, 20, 84, 1, 2, 49, 29, 99, 38, 12, 18 };
+            Write("I:", unsortedArray);
+            MergeSort(unsortedArray, 0, unsortedArray.Length - 1);
+            Write("O:", unsortedArray);
+            Console.ReadLine();
+        }
+
+        private static void MergeSort(int[] unsortedArray, int leftIndex, int rightIndex)
+        {
+            if (leftIndex < rightIndex)
+            {
+                int middleIndex = (leftIndex + rightIndex) / 2;
+                MergeSort(unsortedArray, leftIndex, middleIndex);
+                MergeSort(unsortedArray, middleIndex + 1, rightIndex);
+                Merge(unsortedArray, leftIndex, middleIndex, rightIndex);
+            }
+        }
+
+        private static void Merge(int[] unsortedArray, int leftIndex, int middleIndex, int rightIndex)
+        {
+            int lengthLeft = middleIndex - leftIndex + 1;
+            int lengthRight = rightIndex - middleIndex;
+            int[] leftArray = new int[lengthLeft + 1];
+            int[] rightArray = new int[lengthRight + 1];
+            for (int i = 0; i < lengthLeft; i++)
+            {
+                leftArray[i] = unsortedArray[leftIndex + i];
+            }
+            for (int j = 0; j < lengthRight; j++)
+            {
+                rightArray[j] = unsortedArray[middleIndex + j + 1];
+            }
+            leftArray[lengthLeft] = Int32.MaxValue;
+            rightArray[lengthRight] = Int32.MaxValue;
+            int iIndex = 0;
+            int jIndex = 0;
+            for (int k = leftIndex; k <= rightIndex; k++)
+            {
+                if (leftArray[iIndex] <= rightArray[jIndex])
+                {
+                    unsortedArray[k] = leftArray[iIndex];
+                    iIndex++;
+                }
+                else
+                {
+                    unsortedArray[k] = rightArray[jIndex];
+                    jIndex++;
+                }
+            }
         }
 
         private static void Write(string prefix, int[] array)
